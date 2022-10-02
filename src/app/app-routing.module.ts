7f7bbabc7c811f2940e27754d9d614c5d363b669
Cainer <cainecorwin@gmail.com>
2022-10-02 00:43:58 -0500

    fixed some position and file issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImageDocumentationComponent } from './image-documentation/image-documentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'hell', component:ImageDocumentationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
